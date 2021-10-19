383f1c4e2df921492bf639a0ef83a66b39e094ba
sayani <70441269+saysayani11@users.noreply.github.com>
2021-10-19 21:29:12 +0530

    Update SpeciesInteractionMatrix2Graph.py

--- SpeciesInteractionMatrix2Graph.py
+++ SpeciesInteractionMatrix2Graph.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
-# read the excel (.xls) file
+
+# read the file
 df1 = pd.read_excel (r'C:\Users\saySa\OneDrive\Desktop\Dataset\A_HP_041.xls',index_col=[0])
 
 # Delete the first column that has data on the total number of samples
 del df1['Num. of hosts sampled']
 
-# convert to a list
+# Convert to a list
 df2 = df1.stack().reset_index()
 
 # Get indexes for which column 0 has value 0
@@ -20,6 +21,8 @@ df2.drop(indexNames , inplace=True)
 G=nx.from_pandas_edgelist(df=df2, source='level_0', target='level_1', create_using=nx.Graph())
 nx.draw(G,with_labels =True)
 
+# Host-parasite interaction
+# Analysis
 
 print(nx.info(G), "\n")
 print("Network Density: ",nx.density(G))
